0fdc3350f6fe545bd9ec44ef75efa3771edf19f8
Lucas Ramos <lucas_r@me.com>
2015-01-04 16:12:15 +0100

    build done WTF

---
+++ builds/2/Gruntfile.js
@@ -0,0 +1,25 @@
+var mozjpeg = require('imagemin-mozjpeg');
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    imagemin: {                          // Task
+      dynamic: {                         // Another target
+        options: {                       // Target options
+          optimizationLevel: 3,
+          progressive: true,
+          use: [mozjpeg()]
+        },
+        files: [{
+          expand: true,                 // Enable dynamic expansion
+          cwd: 'old_images/',           // Src matches are relative to this path
+          src: ['**/*.{png,jpg}'],      // Actual patterns to match
+          dest: 'img/'                  // Destination path prefix
+        }]
+      }
+    }
+  });
+}
+
+grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+grunt.registerTask('default', ['imagemin']);
