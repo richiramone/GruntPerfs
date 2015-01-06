b8939e12f2fe388bff363b018a2d889ceb5ab963
Lucas Ramos <lucas_r@me.com>
2015-01-06 21:31:56 +0100

    build 7 done

--- builds/7/Gruntfile.js
+++ builds/7/Gruntfile.js
@@ -1,17 +1,40 @@
 
 module.exports = function(grunt) {
   grunt.initConfig({
-    colorguard: {
-      options: {
-        threshold : 3
-      },
-      files: {
-        src: ['css/style.css'],
+    perfbudget: {
+      default: {
+        options: {
+          url: 'http://lucasramos.me/gruntperfs/builds/7/',
+          key: 'A.d01077156635968a5bd2637fda103bd2',
+          runs: 2
+        }
+      }
+    },
+    pagespeed: {
+      dev: {
+        options: {
+          nokey: true,
+          url: "http://lucasramos.me/gruntperfs/builds/7/index.html",
+          locale: "it_IT",
+          strategy: "desktop",
+          threshold: 70
+        }
+      }
+    },
+    phantomas: {
+      dev : {
+        options : {
+          indexPath : './phantomas/',
+          url       : 'http://lucasramos.me/gruntperfs/builds/7/index.html',
+          buildUi   : true
+        }
       }
     }
   });
-  
-  grunt.loadNpmTasks('grunt-colorguard');
 
-  grunt.registerTask('default', ['colorguard']);
+  grunt.loadNpmTasks('grunt-perfbudget');
+  grunt.loadNpmTasks('grunt-pagespeed');
+  grunt.loadNpmTasks('grunt-phantomas');
+
+  grunt.registerTask('default', ['perfbudget', 'pagespeed', 'phantomas']);
 }
